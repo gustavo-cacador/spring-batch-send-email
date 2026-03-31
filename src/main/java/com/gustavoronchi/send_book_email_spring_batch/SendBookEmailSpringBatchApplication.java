480a94942b1cc436c031c635e4352a166cac7c1a
Gustavo Caçador <gustavoirl456@outlook.com>
2026-03-31 07:31:52 -0300

    Projeto criado

---
+++ src/main/java/com/gustavoronchi/send_book_email_spring_batch/SendBookEmailSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.send_book_email_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendBookEmailSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendBookEmailSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavoronchi/send_book_email_spring_batch/SendBookEmailSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.send_book_email_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendBookEmailSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
