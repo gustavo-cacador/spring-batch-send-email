03a75830864bde827939d9ff3e252c4e5813dff4
Gustavo Caçador <gustavoirl456@outlook.com>
2026-03-31 15:36:39 -0300

    leitura de usuarios a serem notificados

---
+++ src/main/java/com/gustavoronchi/send_book_email_spring_batch/reader/ReadUsersWithLoansCloseToReturnReaderConfig.java
@@ -0,0 +1,61 @@
+package com.gustavoronchi.send_book_email_spring_batch.reader;
+
+import com.gustavoronchi.send_book_email_spring_batch.domain.Book;
+import com.gustavoronchi.send_book_email_spring_batch.domain.User;
+import com.gustavoronchi.send_book_email_spring_batch.domain.UserBookLoan;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.lang.Nullable;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Configuration
+public class ReadUsersWithLoansCloseToReturnReaderConfig {
+
+    int numDaysToNotifyReturn = 6;
+
+    @Bean
+    public ItemReader<UserBookLoan> readUsersWithLoansCloseToReturnReader(
+            @Qualifier("appDB")DataSource dataSource) {
+        return new JdbcCursorItemReaderBuilder<UserBookLoan>()
+                .name("readUsersWithLoansCloseToReturnReader")
+                .dataSource(dataSource)
+                .sql("""
+                        select user.id as user_id,
+                            user.name as user_name,
+                            user.email as user_email,
+                            book.id as book_id,
+                            book.name as book_name,
+                            loan.loan_date
+                        from tb_user_book_loan as loan
+                        inner join tb_user as user on loan.user_id = user.id
+                        inner join tb_book as book on loan.book_id = book.id
+                        where date_add(loan.loan_date, interval ? day) = date(now())
+                        """)
+                .queryArguments(numDaysToNotifyReturn)
+                .rowMapper(rowMapper())
+                .build();
+    }
+
+    private RowMapper<UserBookLoan> rowMapper() {
+        return new RowMapper<UserBookLoan>() {
+            @Nullable
+            @Override
+            public UserBookLoan mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User(rs.getInt("user_id"), rs.getString("user_name"), rs.getString("user_email"));
+                Book book = new Book();
+                book.setId(rs.getInt("book_id"));
+                book.setName(rs.getString("book_name"));
+                UserBookLoan userBookLoan = new UserBookLoan(user, book, rs.getDate("loan_date"));
+                return userBookLoan;
+            }
+        };
+    }
+}
+
