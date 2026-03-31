77a3d84a7b2386aa48fdce0970ee332e7f75d0e7
Gustavo Caçador <gustavoirl456@outlook.com>
2026-03-31 14:53:45 -0300

    configuração inicial Job e Step

---
+++ src/main/java/com/gustavoronchi/send_book_email_spring_batch/step/SendEmailUserStepConfig.java
@@ -0,0 +1,28 @@
+package com.gustavoronchi.send_book_email_spring_batch.step;
+
+import com.gustavoronchi.send_book_email_spring_batch.domain.UserBookLoan;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class SendEmailUserStepConfig {
+
+    @Autowired
+    @Qualifier("transactionManagerApp")
+    private PlatformTransactionManager transactionManager;
+
+    @Bean
+    public Step sendEmailUserStep(ItemReader<UserBookLoan> readUsersWithLoansCloseToReturnReader, JobRepository jobRepository) {
+        return new StepBuilder("sendEmailUserStep", jobRepository)
+                .<UserBookLoan, UserBookLoan>chunk(1, transactionManager)
+                .reader(readUsersWithLoansCloseToReturnReader)
+                .build();
+    }
+}
